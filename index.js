be94aea11dc73226504a4807f11a3cb73efc1bc0
rokuroTM <115215057+rokuroTM@users.noreply.github.com>
2024-10-18 18:51:32 +0200

    - added new feature: now iterates through pages - added comments - updated .gitignore

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const websiteArray = [
     'https://safebooru.org'
 ];
-const query = 'ishmael_(project_moon)';
+const query = 'morgana_(league_of_legends)';
 const downloadFilepath = path.join(__dirname, 'images');
 const pageSearchNumber = 3; //W.I.P.
 
@@ -41,72 +41,103 @@ async function downloadImageFromBooruPage(page, filepath){
 
 async function main(){
     const browser = await puppeteer.launch({
-        headless: 'true'
+        args: ["--no-startup-window", "--no-first-run", "--no-sandbox"],
+        waitForInitialPage: false
     });
     const page = await browser.newPage();
     await page.setViewport({
         width: 1920,
         height: 1080
     });
-    console.log('Launch successful!');
+    console.log('\nLaunch successful!');
 
 
     for (let website of websiteArray){
+        
+        //BUG: reload algorithm doesn't work
+
         for (let attempt_counter = 0; attempt_counter < 3; attempt_counter++){
             try {
                 await page.goto(website);
-                console.log(`\n------------------- ${website} -------------------\n`);
+                console.log(`\n------------------- ${website} -------------------`);
 
+                //writes query in searchbar
                 await page.locator('#tags').fill(query);
 
+                //BUG: query sometimes isn't fully typed in search bar
+
+                //clicks the search button
                 await Promise.all([
-                    page.click('[type=submit]'),
+                    page.locator("[type=submit]").click(),
                     page.waitForNavigation({
                         waitUntil: 'networkidle2'
                     })
                 ]);
-
+                
+                //breaks attempt cycle
                 break;
+
             } catch (err){
                 if (err instanceof puppeteer.TimeoutError){
-                    console.log(`Attempt ${attempt_counter + 1} of 3 failed, reloading page...`);
+                    console.log(`\nAttempt ${attempt_counter + 1} of 3 failed, reloading page...`);
                 } else {
-                    console.error("Error: site could not be reached. Please try again.")    
+                    console.error("\nError: site could not be reached. Please try again.")    
                     break;
                 }
-            };
-        }; 
+            
+            } finally {
+                //testing
+                await page.screenshot({path: `testa${attempt_counter}.png`});
+            }
+        };
         
-        //testing
-        await page.screenshot({path: 'test.png'});
-        
-        const linkList = await page.evaluate(() => {
-            const nodeList = document.querySelectorAll('span > a, .post-preview-link > a');
-            const linkList = [];
-    
-            nodeList.forEach((element) => {
-                linkList.push(element.href);
-            });
-    
-            return linkList;
-        });
-
         for (let pageCounter = 0; pageCounter < pageSearchNumber; pageCounter++){
+            const currentPage = page.url();
+            
+            //testing
+            await page.screenshot({path: `testc${pageCounter}.png`});
+            
+            //gets each image's href (to get the higher quality download) and adds them to a list
+            const linkList = await page.evaluate(() => {
+                const nodeList = document.querySelectorAll('span > a, .post-preview-link > a');
+                const linkList = [];
+    
+                nodeList.forEach((element) => {
+                    linkList.push(element.href);
+                });
+    
+                return linkList;
+            });
+            
+            //iterates through the list and downloads the image from each link
+
             if (linkList.length === 0){
-                console.log("No posts found. Moving to next site...");
-                break;
+                console.log("\nNo posts found. Moving to next site...");
             } else {
                 for (let link of linkList){
                     await page.goto(link);
                     await downloadImageFromBooruPage(page, downloadFilepath);
                 };
-            
-                console.log("All images in page downloaded!");
-                break; //to remove later
+                console.log("\nAll images in page downloaded!");
+                
+                await page.goto(currentPage);
+                await page.evaluate(_ => {
+                    window.scrollBy(0, window.innerHeight);
+                });
+
+                try {
+                    await page.locator('div > a[id*="next"], div > a[alt*="next"]').click({timeout: 5000});
+
+                    //testing
+                    page.screenshot({path: `testd${pageCounter}.png`});
+                } catch(error){
+                    console.log(`Error - ${error}: Next page button not found. Moving to next site...`);
+                    break;
+                };
+
+                
             };
         };
-
-        //TODO: add code to move to next pages
 
     await browser.close();
     };
