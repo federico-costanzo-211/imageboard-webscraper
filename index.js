a183c3b7675229d6fdaca63ceba1942a5d5acb4a
rokuroTM <115215057+rokuroTM@users.noreply.github.com>
2024-10-30 11:46:22 +0100

    fixed bug where prompt would not be fully typed fixed bug where reloading failed multiple times on empty page

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const fs = require('node:fs');
 const path = require('path');
 
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
 const websiteArray = [
-    'https://safebooru.org'
+    'https://safebooru.org', //example site
 ];
-const query = 'morgana_(league_of_legends)';
+const query = '';
 const downloadFilepath = path.join(__dirname, 'images');
-const pageSearchNumber = 3; //W.I.P.
+const pageSearchNumber = 2;
 
 //----------------------------------------------------------
 
@@ -17,86 +20,72 @@ async function downloadImageFromBooruPage(page, filepath){
     };
     
     const imageUrl = await page.evaluate(() => {
-        let image = document.querySelector("#image");
+        let image = document.querySelector('#image');
         return [image.src, image.alt];
     });
 
     const imageBuffer = await fetch(imageUrl[0], {
-        method: "GET",
-        mode: "no-cors"
+        method: 'GET',
+        mode: 'no-cors'
     })
     .then(async (response) => await response.arrayBuffer())
     .then((response) => Buffer.from(response))
     .catch((error) => console.error(error));
     
-    const filename = imageUrl[1].replace(/\W/g, "").substring(0, 250) + ".png";
+    const filename = imageUrl[1].replace(/\W/g, '').substring(0, 250) + '.png';
     console.log(filename);
     fs.writeFileSync(path.join(filepath, filename), imageBuffer, {flag: 'a+'});
 };
 
 //----------------------------------------------------------------
 
-//todo: page browsing
-//todo: imput from user
-
 async function main(){
     const browser = await puppeteer.launch({
-        args: ["--no-startup-window", "--no-first-run", "--no-sandbox"],
+        args: ['--no-startup-window', '--no-first-run', '--no-sandbox'],
+        headless: true,
         waitForInitialPage: false
     });
+    
     const page = await browser.newPage();
     await page.setViewport({
         width: 1920,
         height: 1080
     });
+    
     console.log('\nLaunch successful!');
 
-
     for (let website of websiteArray){
-        
-        //BUG: reload algorithm doesn't work
+        //BUG?: reload algorithm doesn't work
 
         for (let attempt_counter = 0; attempt_counter < 3; attempt_counter++){
             try {
                 await page.goto(website);
                 console.log(`\n------------------- ${website} -------------------`);
 
-                //writes query in searchbar
-                await page.locator('#tags').fill(query);
+                await page.waitForSelector('#tags');
+                await page.type('#tags', query);
 
-                //BUG: query sometimes isn't fully typed in search bar
-
-                //clicks the search button
                 await Promise.all([
-                    page.locator("[type=submit]").click(),
+                    page.locator('[type=submit], #search-box-submit').click(),
                     page.waitForNavigation({
                         waitUntil: 'networkidle2'
                     })
                 ]);
-                
-                //breaks attempt cycle
                 break;
 
             } catch (err){
                 if (err instanceof puppeteer.TimeoutError){
                     console.log(`\nAttempt ${attempt_counter + 1} of 3 failed, reloading page...`);
+                    await page.screenshot({path: "test_page.png"});
                 } else {
-                    console.error("\nError: site could not be reached. Please try again.")    
-                    break;
+                    console.error('\nError: site could not be reached. Please try again.');
                 }
-            
-            } finally {
-                //testing
-                await page.screenshot({path: `testa${attempt_counter}.png`});
             }
         };
         
         for (let pageCounter = 0; pageCounter < pageSearchNumber; pageCounter++){
             const currentPage = page.url();
-            
-            //testing
-            await page.screenshot({path: `testc${pageCounter}.png`});
-            
+
             //gets each image's href (to get the higher quality download) and adds them to a list
             const linkList = await page.evaluate(() => {
                 const nodeList = document.querySelectorAll('span > a, .post-preview-link > a');
@@ -110,37 +99,42 @@ async function main(){
             });
             
             //iterates through the list and downloads the image from each link
-
             if (linkList.length === 0){
-                console.log("\nNo posts found. Moving to next site...");
+                console.log('\nNo posts found. Moving to next site...');
+                break;
             } else {
                 for (let link of linkList){
                     await page.goto(link);
                     await downloadImageFromBooruPage(page, downloadFilepath);
                 };
-                console.log("\nAll images in page downloaded!");
+                
+                console.log('\nAll images in page downloaded!');
                 
                 await page.goto(currentPage);
                 await page.evaluate(_ => {
                     window.scrollBy(0, window.innerHeight);
                 });
 
+                //checks if there is a button to go to the next page
                 try {
-                    await page.locator('div > a[id*="next"], div > a[alt*="next"]').click({timeout: 5000});
-
-                    //testing
-                    page.screenshot({path: `testd${pageCounter}.png`});
-                } catch(error){
-                    console.log(`Error - ${error}: Next page button not found. Moving to next site...`);
+                    await Promise.all([
+                        page.locator('div > a[id*="next"], div > a[alt*="next"]').click({timeout: 5000}),
+                        page.waitForNavigation()
+                    ]);
+                }
+                catch(error){
+                    console.log(`Error (${error}) --- Next page button not found. Moving to next site...`);
                     break;
                 };
-
-                
             };
         };
+    };
 
     await browser.close();
-    };
 };
 
 main();
+
+//TODO: add video downloading support
+//TODO: add captcha bypass
+//TODO: add user input
